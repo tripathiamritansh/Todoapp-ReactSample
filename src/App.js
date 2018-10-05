b5648c6a09d8727accb8a133a9e04b6795be6574
Amritansh Tripathi <tripathi.a@husky.neu.edu>
2018-10-05 13:02:12 -0400

    added feature to input new item in the todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 class App extends Component {
     state = {
@@ -12,10 +13,18 @@ class App extends Component {
     deleteTodo = (id) => {
         const todos = this.state.todos.filter(todo => {
             return todo.id !== id
-        })
+        });
 
         this.setState({
             todos: todos
+        })
+    };
+
+    addTodo = (todo) => {
+        todo.id = Math.random()
+        let todos = [...this.state.todos, todo]
+        this.setState({
+            todos
         })
     };
 
@@ -24,6 +33,7 @@ class App extends Component {
             <div className="todo-app container">
                 <h1 className="center blue-text"> Todo's</h1>
                 <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+                <AddTodo addTodo={this.addTodo}/>
             </div>
         );
     }
